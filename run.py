90d32d87845c92767cebb836d156cced97f21a34
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-04-27 06:59:30 +0000

    Added basic function to set the header row of the sheets and added each sheet variable as empty array for now

--- run.py
+++ run.py
@@ -16,3 +16,33 @@ personal_sheet = SHEET.worksheet("Personal")
 professional_sheet = SHEET.worksheet("Professional")
 emergency_sheet = SHEET.worksheet("Emergency")
 favorites_sheet = SHEET.worksheet("Favorites")
+
+personal_data = []
+
+professional_data = []
+
+emergency_data = []
+
+favorites_data = []
+
+def add_data_with_name_column(sheet, data):
+    """
+    Adds new data to the sheets with a predefined header
+    If the sheet is empty, it sets a header row with its columns on A1 and B1
+    as "Name" and "Telephone Number"
+    
+    Then it appends the data provided by the user to the sheet
+    """
+    existing_data = sheet.get_all_values()
+    print("Existing data:", existing_data)
+
+    if not existing_data or not existing_data[0]:
+        header_row = ["Name", "Telephone Number"]
+        sheet.insert_row(header_row, index=1)
+        header_range = sheet.range("A1:B1")
+        for cell in header_range:
+            cell.textFormat['bold'] = True
+        sheet.update_cells(header_range)
+    else:
+        for row in data:
+            sheet.append_row(row)
